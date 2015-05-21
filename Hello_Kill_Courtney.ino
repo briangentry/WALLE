37d0708a958acf5efb2240665e143493f25f98d5
briangentry <brian.gentry1@gmail.com>
2015-05-21 13:10:44 -0700

    Commit
    Change the #### with ~ to an appropriate pin

---
+++ Hello_Kill_Courtney.ino
@@ -0,0 +1,22 @@
+char message[] = {183, 70, 159, 137, 255};
+char killcourtney[] = {194, 129, 8, 159, 2, 195, 153, 191, 141, 8, 128, 255};
+
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(#### with ~, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  for (int i = 0; i < 5; i++) {
+    int number = message[i];
+    analogWrite(#### with ~, number);
+  }
+  delay(1000);
+  for (int j = 0; j < 12; j++) {
+    int number = killcourtney[j];
+    analogWrite(#### with ~, number);
+  }
+  delay(1000);
+}
