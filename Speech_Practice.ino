6ed5ab393d9636daf5176e4124179c47f48f51a1
briangentry <brian.gentry1@gmail.com>
2015-05-21 14:43:39 -0700

    Renamed

--- Speech_Practice.ino
+++ Speech_Practice.ino
@@ -1,5 +1,5 @@
-char message[] = {183, 70, 159, 137, 255};
-char killcourtney[] = {194, 129, 8, 159, 2, 195, 153, 191, 141, 8, 128, 255};
+char hello[] = {183, 70, 159, 137, 255};
+char surprise[] = {194, 129, 8, 159, 2, 195, 153, 191, 141, 8, 128, 255};
 
 
 void setup() {
@@ -10,12 +10,12 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
   for (int i = 0; i < 5; i++) {
-    int number = message[i];
+    int number = hello[i];
     analogWrite(#### with ~, number);
   }
   delay(1000);
   for (int j = 0; j < 12; j++) {
-    int number = killcourtney[j];
+    int number = surprise[j];
     analogWrite(#### with ~, number);
   }
   delay(1000);
