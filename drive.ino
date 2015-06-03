ef002be87b2931b99b7cd6ea2e476e69edae689c
rylandjackreamy <rylandjackreamy@users.noreply.github.com>
2015-06-02 20:05:49 -0700

    Update drive.ino

--- drive.ino
+++ drive.ino
@@ -1,9 +1,26 @@
+#include <Ultrasonic.h>
+
+Ultrasonic ultra(4, 2);
+
+float ultraSonic(){
+  return ultra.Timing() / 29.1 / 2;
+}
+
 void forward(){
   digitalWrite(7, HIGH);
   digitalWrite(8, LOW);
   digitalWrite(13, LOW); // direction right
   digitalWrite(12, LOW); // direction left
-  analogWrite(3, 130); // motor left
+  analogWrite(3, 85); // motor left
+  analogWrite(11, 255); // motor rigth
+}
+
+void backward(){
+  digitalWrite(7, HIGH);
+  digitalWrite(8, LOW);
+  digitalWrite(13, HIGH); // direction right
+  digitalWrite(12, HIGH); // direction left
+  analogWrite(3, 85); // motor left
   analogWrite(11, 255); // motor rigth
 }
 
@@ -43,16 +60,63 @@ void setup() {
   pinMode(3, OUTPUT);
   pinMode(11, OUTPUT);
   pinMode(12, OUTPUT);
+  Serial.begin(115200);
+}
+
+void drive(){
+  forward();
+  while(ultraSonic() > 25){}
+  stopp();
+  delay(100);
+  backward();
+  delay(250);
+  stopp();
+  int dir = newPath();
+  if (dir > 0){
+    left(dir);
+  } else {
+    right(-1 * dir);
+  }
+}
+
+void rotate(float time){
+  digitalWrite(7, LOW);
+  digitalWrite(8, HIGH);
+  analogWrite(3, 255);
+  delay(time);
+  analogWrite(3, 0);
+  digitalWrite(8, LOW);
+  delay(500);
+}
+
+float newPath(){
+  digitalWrite(12, LOW);
+  rotate(48.0);
+  digitalWrite(12, HIGH);
+  int distance = 0;
+  float pause, u;
+  int nAngle;
+  delay(1000);
+  short pauses[] = {5, 6, 8, 10, 14, 24};
+  for (int i = -90; i < 90; i += 30){
+    u = ultraSonic();
+    if (distance < u){
+      nAngle = i;
+      distance = u;
+    }
+    rotate(pauses[(i+90)/30]);
+    delay(90);
+  }
+  if (distance < ultraSonic()){
+    nAngle = 90;
+    distance = ultraSonic();
+  }
+  digitalWrite(12, LOW);
+  rotate(25.0);
+  return nAngle;
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  left(30);
-  delay(1000);
-  left(60);
-  delay(1000);
-  left(90);
-  delay(1000);
-  left(180);
-  delay(1000);
+  drive();
 }
